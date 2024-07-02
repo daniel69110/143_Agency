26ebd8d2b67442afcda39d8792eb3c15ec2f6ea4
Daniel-COUROT <danielcourot@hotmail.com>
2024-07-02 14:16:42 +0200

    final

--- listings/bdo.php
+++ listings/bdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new \PDO('mysql:host=localhost;dbname=fmdh;port=3308;charset=utf8', 'root', 'dwwm_2024', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
 
 
 $sql ='SELECT * FROM users';
